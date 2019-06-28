f24069c6b5ec77c802bb684373b749544326df19
Rick Mann <rmann@latencyzero.com>
2019-06-27 17:23:52 -0700

    First commit.

---
+++ SwiftImgur/Imgur.h
@@ -0,0 +1,19 @@
+//
+//  Imgur.h
+//  Imgur
+//
+//  Created by Rick Mann on 2019-06-27.
+//  Copyright © 2019 Latency: Zero, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Imgur.
+FOUNDATION_EXPORT double ImgurVersionNumber;
+
+//! Project version string for Imgur.
+FOUNDATION_EXPORT const unsigned char ImgurVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Imgur/PublicHeader.h>
+
+
